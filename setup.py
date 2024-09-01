30a1fc3f3203bf32d1100bc9ac9d8b1f63de3bce
Shubham Vishwakarma <sv09734@gmail.com>
2024-09-01 11:52:15 +0530

    initial setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='practice project 2',
+    author='shubham-vishwakarma',
+    license='MIT',
+)
